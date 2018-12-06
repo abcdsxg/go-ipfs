64d38d367e8431a3f3a04e5e19ac042994a4c834
Łukasz Magiera <magik6k@gmail.com>
2018-12-06 21:00:40 +0100

    coreapi: Untangle from core.IpfsNode
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- core/coreapi/coreapi.go
+++ core/coreapi/coreapi.go
@@ -15,11 +15,26 @@ package coreapi
 
 import (
 	"context"
+	"errors"
 
 	core "github.com/ipfs/go-ipfs/core"
 	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
+	options "github.com/ipfs/go-ipfs/core/coreapi/interface/options"
+	namesys "github.com/ipfs/go-ipfs/namesys"
+	pin "github.com/ipfs/go-ipfs/pin"
+	repo "github.com/ipfs/go-ipfs/repo"
 
+	ci "gx/ipfs/QmNiJiXwWE3kRhZrC5ej3kSjWHm337pYfhjLGSCDNKJP2s/go-libp2p-crypto"
+	exchange "gx/ipfs/QmP2g3VxmC7g7fyRJDj1VJ72KHZbJ9UW24YjSWEj1XTb4H/go-ipfs-exchange-interface"
+	pstore "gx/ipfs/QmQAGG1zxfePqj2t7bLxyN8AFccZ889DDR9Gn8kVLDrGZo/go-libp2p-peerstore"
+	blockstore "gx/ipfs/QmS2aqUZLJp8kF1ihE5rvDGE5LvmKDPnx32w9Z1BW9xLV5/go-ipfs-blockstore"
+	record "gx/ipfs/QmSoeYGNm8v8jAF49hX7UwHwkXjoeobSrn9sya5NPPsxXP/go-libp2p-record"
+	bserv "gx/ipfs/QmVDTbzzTwnuBwNbJdhW3u7LoBQp46bezm9yp4z1RoEepM/go-blockservice"
+	routing "gx/ipfs/QmZBH87CAPFHcc7cYmBqeSQ98zQ3SX9KUxiYgzPmLWNVKz/go-libp2p-routing"
+	p2phost "gx/ipfs/QmahxMNoNuSsgQefo9rkpcfRFmQrMN6Q99aztKXf63K7YJ/go-libp2p-host"
+	pubsub "gx/ipfs/Qmc3BYVGtLs8y3p4uVpARWyo3Xk2oCBFF1AhYUVMPWgwUK/go-libp2p-pubsub"
 	ipld "gx/ipfs/QmcKKBwfz6FyQdHR2jsXrrF6XeSBXYL86anmWNewpFpoF5/go-ipld-format"
+	peer "gx/ipfs/QmcqU6QUDSXprb1518vYDGczrTJTyGwLG9eUa5iNX4xUtS/go-libp2p-peer"
 	logging "gx/ipfs/QmcuXC5cxs79ro2cUuHs4HQ2bkDLJUYokwL8aivcX6HW3C/go-log"
 	dag "gx/ipfs/QmdURv6Sbob8TVW2tFFve9vcEWrSUgwPqeqnXyvYhLrkyd/go-merkledag"
 )
@@ -27,13 +42,76 @@ import (
 var log = logging.Logger("core/coreapi")
 
 type CoreAPI struct {
-	node *core.IpfsNode
-	dag  ipld.DAGService
+	nctx context.Context
+
+	identity   peer.ID //TODO: check mutable structs
+	privateKey ci.PrivKey
+
+	repo       repo.Repo
+	blockstore blockstore.GCBlockstore
+	baseBlocks blockstore.Blockstore
+	blocks     bserv.BlockService
+	dag        ipld.DAGService
+	pinning    pin.Pinner
+
+	peerstore       pstore.Peerstore
+	peerHost        p2phost.Host
+	namesys         namesys.NameSystem
+	recordValidator record.Validator
+	exchange        exchange.Interface
+
+	routing routing.IpfsRouting
+	pubSub  *pubsub.PubSub
+
+	checkRouting func(bool) error
+
+	// TODO: this can be generalized to all functions when we implement some
+	// api based security mechanism
+	isPublishAllowed func() error
 }
 
 // NewCoreAPI creates new instance of IPFS CoreAPI backed by go-ipfs Node.
-func NewCoreAPI(n *core.IpfsNode) coreiface.CoreAPI {
-	api := &CoreAPI{n, n.DAG}
+func NewCoreAPI(n *core.IpfsNode, opts ...options.ApiOption) coreiface.CoreAPI {
+	api := &CoreAPI{
+		nctx: n.Context(),
+
+		identity:   n.Identity,
+		privateKey: n.PrivateKey,
+
+		repo:       n.Repo,
+		blockstore: n.Blockstore,
+		baseBlocks: n.BaseBlocks,
+		blocks:     n.Blocks,
+		dag:        n.DAG,
+		pinning:    n.Pinning,
+
+		peerstore:       n.Peerstore,
+		peerHost:        n.PeerHost,
+		namesys:         n.Namesys,
+		recordValidator: n.RecordValidator,
+		exchange:        n.Exchange,
+
+		routing: n.Routing,
+		pubSub:  n.PubSub,
+
+		checkRouting: func(allowOffline bool) error {
+			if !n.OnlineMode() {
+				if !allowOffline {
+					return coreiface.ErrOffline
+				}
+				return n.SetupOfflineRouting()
+			}
+			return nil
+		},
+
+		isPublishAllowed: func() error {
+			if n.Mounts.Ipns != nil && n.Mounts.Ipns.IsActive() {
+				return errors.New("cannot manually publish while IPNS is mounted")
+			}
+			return nil
+		},
+	}
+
 	return api
 }
 
@@ -89,6 +167,10 @@ func (api *CoreAPI) PubSub() coreiface.PubSubAPI {
 
 // getSession returns new api backed by the same node with a read-only session DAG
 func (api *CoreAPI) getSession(ctx context.Context) *CoreAPI {
-	ng := dag.NewReadOnlyDagService(dag.NewSession(ctx, api.dag))
-	return &CoreAPI{api.node, ng}
+	sesApi := *api
+
+	//TODO: we may want to apply this to other things too
+	sesApi.dag = dag.NewReadOnlyDagService(dag.NewSession(ctx, api.dag))
+
+	return &sesApi
 }
